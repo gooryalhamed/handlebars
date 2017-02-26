a1b8e68b374be1fe760ede77bf4e0eef8c882f77
Goory al Hamed <gooryalhamed@hotmail.com>
2017-02-26 22:13:28 +0300

    initial comm

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var ourRequest =new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+ourRequest.onload = function(){
+	if (ourRequest.status >= 200 && ourRequest.status < 400){
+		var ourData = JSON.parse(ourRequest.responseText);
+		createHTML(ourData);
+	}else{
+		console.log("unable to retrieve data");
+	}
+	
+};
+ourRequest.onerror = function(){
+	console.log("connection error");
+}
+ourRequest.send();
+function createHTML(petsData){
+	var rowTemplate = document.getElementById("pets-template").innerHTML;
+	var compiledTemplate = Handlebars.compile(rowTemplate);
+	var ourGeneratedHTML = compiledTemplate(petsData);
+	var petsContainer = document.getElementById("pets-container");
+	petsContainer.innerHTML = ourGeneratedHTML;
+}
